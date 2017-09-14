a380736b6d015b69f25a31aeb537d2e3a4c0582d
Ian Haken <ihaken@netflix.com>
2017-09-13 19:40:52 -0700

    Add ruby test and results.

---
+++ testsuites/ruby_test.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+require 'net/http'
+require 'net/https'
+
+if ARGV[0] == '--useragent'
+  puts "Ruby: #{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}, #{OpenSSL::OPENSSL_VERSION}"
+  exit(0)
+end
+
+url = URI(ARGV[0])
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.ca_file = '../certificates/root.crt'
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+begin
+  http.get(url.path)
+rescue => e
+  exit(1)
+end
+
+exit(0)
