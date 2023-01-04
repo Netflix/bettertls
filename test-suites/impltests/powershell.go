9d0878efcc23fb7e46090610d93c24f1f0f31e0e
Jeremy Rand <jeremyrand@danwin1210.de>
2023-01-04 04:05:53 +0000

    Embed PowerShell script in powershell.go

--- test-suites/impltests/powershell.go
+++ test-suites/impltests/powershell.go
@@ -3,14 +3,17 @@ package impltests
 import (
 	"fmt"
 	test_executor "github.com/Netflix/bettertls/test-suites/test-executor"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 // Compatibility notes:
 // This runner requires Administrator privileges so that it can add/remove root CA's.
-// You must also have "test-suites/scripts/" in your PATH.
 
 type PowerShellRunner struct {
 	version string
+	tmpDir  string
 }
 
 func (c *PowerShellRunner) Name() string {
@@ -23,10 +26,60 @@ func (c *PowerShellRunner) Initialize() error {
 	if err != nil {
 		return err
 	}
+
+	c.tmpDir, err = ioutil.TempDir("", "")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filepath.Join(c.tmpDir, "try-tls-handshake.ps1"), []byte(`
+# Warning!  This script must be run in a fresh PowerShell process.  Otherwise,
+# PowerShell will cache any successful cert validation results, so you'll be
+# getting fictitious results.
+
+param (
+  $url,
+  $capath
+)
+
+$caname = ((& "certutil.exe" "-f" "-enterprise" "-addstore" "Root" "$capath" | Select-String -Pattern 'Certificate ".*"').Matches[0].Value | Select-String -Pattern '".*"').Matches[0].Value.Trim('"')
+If (!$?) {
+  Write-Host "certificate trust failed"
+  exit 1
+}
+
+try {
+  Invoke-WebRequest -Uri "$url" -Method GET -UseBasicParsing
+  $success = $?
+}
+catch {
+  $success = $false
+}
+
+& "certutil.exe" "-enterprise" "-delstore" "Root" "$caname"
+If (!$?) {
+  Write-Host "certificate untrust failed"
+  exit 1
+}
+
+if ($success) {
+  exit 0
+}
+
+exit 1
+`), 0644)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (c *PowerShellRunner) Close() error {
+	if c.tmpDir != "" {
+		return os.RemoveAll(c.tmpDir)
+	}
+
 	return nil
 }
 
@@ -37,7 +90,7 @@ func (c *PowerShellRunner) GetVersion() string {
 func (c *PowerShellRunner) RunTests(ctx *test_executor.ExecutionContext) (map[string]*test_executor.SuiteTestResults, error) {
 	return testExec(ctx, func(caPath string, hostname string, tlsPort uint) []string {
 		return []string{
-			"powershell", "-ExecutionPolicy", "Unrestricted", "-Command", "try-tls-handshake.ps1", "-url", fmt.Sprintf("https://%s:%d/ok", hostname, tlsPort), "-capath", caPath,
+			"powershell", "-ExecutionPolicy", "Unrestricted", "-File", filepath.Join(c.tmpDir, "try-tls-handshake.ps1"), "-url", fmt.Sprintf("https://%s:%d/ok", hostname, tlsPort), "-capath", caPath,
 		}
 	})
 }
